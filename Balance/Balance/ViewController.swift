e65fba59ccafc18802a2380b6077e161be5fad4d
Russell Gordon <rgordon@rsgc.on.ca>
2017-02-19 07:32:05 -0500

    Now shows list of students, resolves #2

--- Balance/Balance/ViewController.swift
+++ Balance/Balance/ViewController.swift
@@ -8,9 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     // MARK: Properties
+    var studentNames = ["Bagga, Puneet", "Blackwell, Scott", "Byrne, Liam", "Elder, Andrew", "Goldsmith, Jeffrey", "Jones, Nicholas", "Leder, Brendan", "McCutcheon, Mark", "Noble Curveira, Carlos"]
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
@@ -24,6 +26,29 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: UITableViewDataSource protocol required methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        // Return the number of rows in the section
+        return studentNames.count
+
+    }
+    
+    // Control the content of the cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // Reuse cells while scrolling to maintain high performance
+        let cellIdentifier = "Cell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        
+        // Configure the cell
+        cell.textLabel?.text = studentNames[indexPath.row]
+        cell.detailTextLabel?.text = "0:00" // Placeholder for now
+        
+        // Return the cell for use in the table view
+        return cell
     }
 
 
