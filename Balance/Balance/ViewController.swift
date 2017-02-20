07fc3a068312b73b6e3854bf357729df6a161783
Russell Gordon <rgordon@rsgc.on.ca>
2017-02-20 18:17:06 -0500

    Simplified calculation of hues, progress on #4
    TODO: Make equal to average best case (120 degrees or green) and above or below moving to red.

--- Balance/Balance/ViewController.swift
+++ Balance/Balance/ViewController.swift
@@ -14,9 +14,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     var students : [Student] = []
     var timer : Timer = Timer()
     var currentSpeaker : Int = -1
+    var activeSpeakers = 0
     var totalTime : Int = 0
-    var maxTime : Int = 0
-    var minTime : Int = 0
     var discussionActive : Bool = false
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var buttonEndDiscussion: UIButton!
@@ -107,16 +106,16 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         if discussionActive {
             
+            // Track number of active speakers
+            if students[currentSpeaker].seconds == 0 {
+                activeSpeakers += 1
+            }
+            
             // Update the model
             students[currentSpeaker].seconds += 1
             
             // Track total discussion time
             totalTime += 1
-            
-            // Track longest speaking time
-            if students[currentSpeaker].seconds > maxTime {
-                maxTime = students[currentSpeaker].seconds
-            }
             
             // Update cell colour for all speakers
             updateStats()
@@ -132,42 +131,22 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     func updateStats() {
         
         // Get the average time per student
-        let averageSpeakingTime = Float(totalTime) / Float(students.count)
-        
-        // Get the overall range
-        let range = Float(maxTime - minTime)
-        
-        // Get the overall range above average
-        let aboveRange = Float(range - averageSpeakingTime)
-        
-        // Get the overall range below average
-        let belowRange = Float(range - aboveRange)
+        let averageSpeakingTime = Float(totalTime) / Float(activeSpeakers)
         
         // Update the colour for each student
         for (index, student) in students.enumerated() {
             
             // Get this student's hue
-            let offset = Float(student.seconds) - averageSpeakingTime
-            if offset < 0 {
-                
-                // Percentage below average within below average range
-                let percentBelow = -offset/belowRange
-                let degreesBelow = 120 * percentBelow
-                student.hue = 240 - degreesBelow + 120
-                
-            } else {
-                
-                // Percentage above average within above average range
-                let percentAbove = offset/aboveRange
-                let degreesAbove = 120 * percentAbove
-                student.hue = 120 - degreesAbove
+            var percentOfAverage = Float(student.seconds) / averageSpeakingTime
+            if percentOfAverage > 2 {
+                percentOfAverage = 2
             }
+            student.hue = 240 - 120 * percentOfAverage
             updateColor(for: index)
         }
         
         
     }
-    
     
     // Updates the background colour for a speaker
     func updateColor(for speaker : Int, saturation : Float = 0.5) {
@@ -176,23 +155,28 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let nsIndexPath = NSIndexPath(row: speaker, section: 0)
         if let indexPath = nsIndexPath as? IndexPath {
             
-            // Update the cell in the table with the new color
-            if let cell = tableView.cellForRow(at: indexPath) {
+            // Update cell colour if student has been active
+            if students[speaker].seconds > 0 {
                 
-                // Calculate the hue
-                let hue = CGFloat(students[speaker].hue)/360
-                
-                // Change colour when cell is not the currently selected cell
-                UIView.beginAnimations(nil, context: nil)
-                UIView.setAnimationDuration(0.5)
-                cell.backgroundColor = UIColor(hue: hue, saturation: CGFloat(saturation), brightness: 1/100*50, alpha: 1/100*100)
-                UIView.commitAnimations()
-                
-                // Change colour when cell IS the currently selected cell
-                UIView.beginAnimations(nil, context: nil)
-                UIView.setAnimationDuration(0.5)
-                cell.selectedBackgroundView?.layer.backgroundColor = UIColor(hue: hue, saturation: CGFloat(saturation), brightness: 1/100*50, alpha: 1/100*100).cgColor
-                UIView.commitAnimations()
+                // Update the cell in the table with the new color
+                if let cell = tableView.cellForRow(at: indexPath) {
+                    
+                    // Calculate the hue
+                    let hue = CGFloat(students[speaker].hue)/360
+                    print(students[speaker].hue)
+                    
+                    // Change colour when cell is not the currently selected cell
+                    UIView.beginAnimations(nil, context: nil)
+                    UIView.setAnimationDuration(0.5)
+                    cell.backgroundColor = UIColor(hue: hue, saturation: CGFloat(saturation), brightness: 1/100*50, alpha: 1/100*100)
+                    UIView.commitAnimations()
+                    
+                    // Change colour when cell IS the currently selected cell
+                    UIView.beginAnimations(nil, context: nil)
+                    UIView.setAnimationDuration(0.5)
+                    cell.selectedBackgroundView?.layer.backgroundColor = UIColor(hue: hue, saturation: CGFloat(saturation), brightness: 1/100*50, alpha: 1/100*100).cgColor
+                    UIView.commitAnimations()
+                }
                 
             }
             
@@ -288,8 +272,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         // Update discussion status and detail
         totalTime = 0
-        minTime = 0
-        maxTime = 0
         labelDiscussionStatus.text = "0:00"
         
     }
